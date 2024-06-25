7da4c6068acae184784b4c8b3bc738b149104e19
Gabriel Gomes Maiworm <65177591+gabrielmaiworm@users.noreply.github.com>
2024-06-25 09:41:44 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,13 +10,51 @@ app.use(express.json());
 
 app.get('/api/searchGooglePlaces', async (req, res) => {
     const query = req.query.query;
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(query)}&key=${apiKey}`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(query)}&key=${apiKey}&language=pt-BR`;
 
     try {
         const response = await axios.get(apiUrl);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar dados do Google Places API' });
+    }
+});
+
+app.get('/api/nearbySearch', async (req, res) => {
+    const { lat, lng, radius, category } = req.query;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=${category}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar dados do Google Places API' });
+    }
+});
+
+app.get('/api/details', async (req, res) => {
+    const placeId = req.query.placeId;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}&language=pt-BR`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar dados do Google Places API' });
+    }
+});
+
+app.get('/api/placePhoto', async (req, res) => {
+    const photoReference = req.query.photoReference;
+    const maxWidth = req.query.maxWidth || 400; // Default maxWidth to 400 if not provided
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoReference}&maxwidth=${maxWidth}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+        res.set('Content-Type', 'image/jpeg');
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar imagem do Google Places API' });
     }
 });
 
